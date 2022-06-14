f3ea9b59c508efda70c3a54603479595ea8311b0
Yvan Nollet <yvan.nollet@rayference.eu>
2022-06-14 13:20:46 +0200

    Implement command-line interface

--- src/ussa1976/__main__.py
+++ src/ussa1976/__main__.py
@@ -1,11 +1,59 @@
 """Command-line interface."""
 import click
+import numpy as np
+
+from .core import compute
 
 
 @click.command()
+@click.option(
+    "--zstart",
+    "-z",
+    help="Start altitude [m]",
+    default=0.0,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--zstop",
+    "-Z",
+    help="Stop altitude [m]",
+    default=1000000.0,
+    show_default=True,
+    type=float,
+)
+@click.option(
+    "--znum",
+    "-n",
+    help="Number of altitude points",
+    default=1001,
+    show_default=True,
+    type=int,
+)
+@click.option(
+    "--filename",
+    "-f",
+    help="Output file name",
+    default="ussa1976.nc",
+    show_default=True,
+    type=click.Path(writable=True),
+)
 @click.version_option()
-def main() -> None:
-    """Ussa1976."""
+def main(
+    zstart: float,
+    zstop: float,
+    znum: int,
+    filename: str,
+) -> None:
+    """Compute the U.S. Standard Atmosphere 1976."""
+    click.echo(
+        f"Computing U.S. Standard Atmosphere 1976 between {zstart} and "
+        f"{zstop} meter on {znum} altitude points."
+    )
+    z = np.linspace(zstart, zstop, znum)
+    ds = compute(z=z)
+    click.echo(f"Writing results in {filename}")
+    ds.to_netcdf(filename)
 
 
 if __name__ == "__main__":
